4e7dc7840a3214ac2092b6fd407218a99a7e1a57
horfius <horfius@gmail.com>
2015-04-03 16:12:14 -0400

    And finally fixed this.

---
+++ src/main/java/com/technostar/tcbot/Launcher.java
@@ -0,0 +1,11 @@
+package com.technostar.tcbot;
+
+/**
+ * Main class for launching or relaunching the bot and all of it's other components.
+ */
+public class Launcher {
+
+    public static void main(String[] args){
+
+    }
+}
