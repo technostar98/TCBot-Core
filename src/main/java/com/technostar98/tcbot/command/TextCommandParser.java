6acb720da2e372230703bfc539295ab1d4c23075
horfius <horfius@gmail.com>
2015-04-26 17:08:05 -0400

    Forgot this one!

---
+++ src/main/java/com/technostar98/tcbot/command/TextCommandParser.java
@@ -0,0 +1,12 @@
+package com.technostar98.tcbot.command;
+
+import java.util.HashMap;
+
+public class TextCommandParser {
+
+    public static String parseCommand(String commandPlain, String message, HashMap<String, Object> channelValues){
+        String parsedMessage = "";
+
+        return parsedMessage;
+    }
+}
